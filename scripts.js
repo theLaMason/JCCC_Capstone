eba2bb42787372b3593ec5a620955bd67d40f18f
theLAMASON <latefamason@yahoo.com>
2025-03-24 21:08:35 -0500

    initial commit

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
-alert('This is an alert from the external script!');
+// Variables 
+let slideIndex= 0;
+const slides = document.querySelectorAll ('.slide');
+const modual = document.getElementById ('modual');
+const modualImg= document.getElementById('modual-img');
+const closeModual= document.getElementById('close-modual');
+
+//Functions
+function showSlides() {
+    for (let i = 0; i <slides.length; i++) {
+       slides[i].style.display= "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+}
+
+//Start slideshow
+setInterval (showSlides, 3000);
+
+//Pause slideshow on hover 
+const slideshowContainer = document.getElementById('slideshow-container');
